ed47679e0e5bd162f9701d5b2c37e7d50c7472f8
alyonaVet <a.v.alenka2015@gmail.com>
2024-06-07 00:48:34 +0600

    created component UserItem;

--- Homework57/src/components/Users/UserItem.tsx
+++ Homework57/src/components/Users/UserItem.tsx
@@ -1,10 +1,19 @@
+import {User} from '../../types';
+import React from 'react';
 
-const UserItem = () => {
+interface useItemProps {
+  user: User;
+}
+const UserItem: React.FC<useItemProps> = ({user}) => {
   return (
-    <div>
-      UserItem
-    </div>
-  );
+    <li className="list-group-item d-flex justify-content-between align-items-start">
+      <div className="ms-2 me-auto">
+        <div>Name: <span className="fw-bold">{user.name}</span></div>
+        <div className="">Email: <span className="fw-bold">{user.email}</span></div>
+        <div className="">Role: <span className="fw-bold">{user.role}</span></div>
+      </div>
+      <span className="badge text-bg-primary rounded-pill">{user.isActive ? 'Active' : 'Not active'}</span>
+    </li>);
 };
 
 export default UserItem;
