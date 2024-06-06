38091b58be5ad59beff33dc224818bdc4ae6d603
alyonaVet <a.v.alenka2015@gmail.com>
2024-06-06 20:41:07 +0600

    created UserForm; added addUser to state;

--- Homework57/src/App.tsx
+++ Homework57/src/App.tsx
@@ -1,14 +1,23 @@
 import './App.css';
 import UserForm from './components/UserForm/UserForm';
 import Users from './components/Users/Users';
+import {useState} from 'react';
+import {User} from './types';
 
 const App = () => {
+
+  const [users, setUsers] = useState<User[]>([]);
+
+  const addUser = (user: User) => {
+    setUsers((prev) => [...prev, user]);
+  };
+  console.log(users);
 
   return (
     <main className="container mt-4">
       <div className="row">
         <div className="col-6">
-          <UserForm />
+          <UserForm onSubmit={addUser}/>
         </div>
         <div className="col-6">
           <Users />
--- Homework57/src/components/UserForm/UserForm.tsx
+++ Homework57/src/components/UserForm/UserForm.tsx
@@ -1,9 +1,104 @@
+import React, {useState} from 'react';
+import {User, UserFormData} from '../../types';
 
-const UserForm = () => {
+interface UserDataProps {
+  onSubmit: (user: User) => void;
+}
+
+const initialState: UserFormData = {
+  name: '',
+  email: '',
+  isActive: false,
+  role: '',
+};
+const UserForm: React.FC<UserDataProps> = ({onSubmit}) => {
+
+  const [userFormData, setUserFormData] = useState<UserFormData>(initialState);
+
+  const changeUser = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    setUserFormData((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const changeIsActive = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserFormData((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.checked,
+    }));
+  };
+
+  const onFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    onSubmit({
+      id: Math.random().toString(),
+      ...userFormData
+    });
+    setUserFormData({
+      name: '',
+      email: '',
+      isActive: false,
+      role: '',
+    });
+  };
+
   return (
-    <div>
+    <form onSubmit={onFormSubmit}>
       <h4 className="text-center">Add user</h4>
-    </div>
+      <div className="form-group">
+        <label htmlFor="name" className="mb-2 mt-2">Name</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          className="form-control"
+          required
+          onChange={changeUser}
+          value={userFormData.name}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="email" className="mb-2 mt-2">Email</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          className="form-control"
+          required
+          onChange={changeUser}
+          value={userFormData.email}
+        />
+      </div>
+      <div className="form-group">
+        <label className="form-check-label mb-2 mt-2" htmlFor="role">Select your role</label>
+        <select
+          name="role"
+          id="role"
+          className="form-select"
+          onChange={changeUser}
+          required
+          value={userFormData.role}
+        >
+          <option value="user">User</option>
+          <option value="editor">Editor</option>
+          <option value="admin">Admin</option>
+        </select>
+      </div>
+      <div className="form-check form-check-reverse mb-2 mt-2">
+        <label htmlFor="isActive" className="form-check-label">Active</label>
+        <input
+          type="checkbox"
+          name="isActive"
+          id="isActive"
+          className="form-check-input"
+          onChange={changeIsActive}
+          checked={userFormData.isActive}
+          required
+        />
+      </div>
+      <button type="submit" className="btn btn-primary mt-3">Create</button>
+    </form>
   );
 };
 
